677219a3e424f620569ca700344699be5efd9c0d
J. David Mendoza <jdmendoza@swau.edu>
2014-04-29 16:18:28 -0500

    Added transactional annotation

--- src/main/java/org/davidmendoza/hyperion/dao/impl/UserDaoHibernate.java
+++ src/main/java/org/davidmendoza/hyperion/dao/impl/UserDaoHibernate.java
@@ -85,6 +85,7 @@ public class UserDaoHibernate extends BaseDao implements UserDao {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Role getRole(String authority) {
         return (Role) currentSession().get(Role.class, authority);
     }
